bd92a6a29237a3c676ee57d46f7b767219f65d2b
Igor Šula <sulaigor@gmail.com>
2019-12-31 08:51:33 +0100

    Change container for address in contact columns components.

--- src/components/ContactColumns/ContactColumns.tsx
+++ src/components/ContactColumns/ContactColumns.tsx
@@ -34,14 +34,14 @@ const ContactColumns = (props: ContactColumnsProps) => {
         <div className={'contactColumns__list d-flex justify-content-between'}>
           <div className={'contactColumns__col'}>
             {firstTitle && <h3>{firstTitle}</h3>}
-            {firstAddress && <h4 className={'contactColumns__col__address'}>{firstAddress}</h4>}
+            {firstAddress && <ReactMarkdown className={'contactColumns__col__address'} source={firstAddress} />}
             {firstEmail &&
               <h5>E-mail: <a style={{ color: '#e50000' }} href={`mailto:${firstEmail}`}>{firstEmail}</a></h5>}
             {firstAccounts && <ReactMarkdown source={firstAccounts} />}
           </div>
           <div className={'contactColumns__col'}>
             {secondTitle && <h3>{secondTitle}</h3>}
-            {secondAddress && <h4 className={'contactColumns__col__address'}>{secondAddress}</h4>}
+            {secondAddress && <ReactMarkdown className={'contactColumns__col__address'} source={secondAddress} />}
             {secondEmail &&
               <h5>E-mail: <a style={{ color: '#e50000' }} href={`mailto:${secondEmail}`}>{secondEmail}</a></h5>}
             {secondAccounts && <ReactMarkdown source={secondAccounts} />}
