7b2b60f8d6098b79cdf394544c7c67e1acc768dd
Petr <pesusak@gmai.com>
2020-03-18 15:53:21 +0100

    build

--- build/components/Footer/Footer.js
+++ build/components/Footer/Footer.js
@@ -36,7 +36,6 @@ var ReactMarkdown = require("react-markdown");
 var Link_1 = require("../../partials/Link");
 var Loader_1 = require("../../partials/Loader");
 var CookiePopup_1 = require("./components/CookiePopup");
-var ValidationAlert_1 = require("../ValidationAlert");
 var GET_CONTEXT = graphql_tag_1.default(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  {\n    languageData @client\n    pageData @client\n    websiteData @client\n    languagesData @client\n    navigationsData @client\n  }\n"], ["\n  {\n    languageData @client\n    pageData @client\n    websiteData @client\n    languagesData @client\n    navigationsData @client\n  }\n"])));
 var GET_PAGES_URLS = graphql_tag_1.default(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  query pagesUrls($language: ID!, $websiteId: ID!) {\n    pagesUrls(where: { language: $language, websiteId: $websiteId }) {\n      id\n      page\n      url\n      name\n      description\n    }\n  }\n"], ["\n  query pagesUrls($language: ID!, $websiteId: ID!) {\n    pagesUrls(where: { language: $language, websiteId: $websiteId }) {\n      id\n      page\n      url\n      name\n      description\n    }\n  }\n"])));
 var CREATE_SUBSCRIBER = graphql_tag_1.default(templateObject_3 || (templateObject_3 = __makeTemplateObject(["\n  mutation($email: String!, $url: String!) {\n    createSubscriber(data: { email: $email, url: $url }) {\n      id\n    }\n  }\n"], ["\n  mutation($email: String!, $url: String!) {\n    createSubscriber(data: { email: $email, url: $url }) {\n      id\n    }\n  }\n"])));
@@ -151,20 +150,6 @@ var Footer = /** @class */ (function (_super) {
                 React.createElement("footer", { className: 'footer' },
                     React.createElement(CookiePopup_1.default, null),
                     React.createElement("div", { className: 'container' },
-                        React.createElement("div", { className: 'footer__newsletter' },
-                            React.createElement("h3", null, "Divesoft newsletter"),
-                            React.createElement("p", null, "Enter your e-mail to subscribe to our newsletter!"),
-                            React.createElement("input", { type: 'e-mail', value: _this.state.email, onChange: function (e) { return onChangeEmail(e); }, placeholder: 'e-mail' }),
-                            React.createElement(react_apollo_1.Mutation, { mutation: CREATE_SUBSCRIBER }, function (createSubscriber) {
-                                return React.createElement("button", { onClick: onSubmit(createSubscriber), className: 'btn' }, "OK");
-                            }),
-                            _this.state.error.email &&
-                                React.createElement(ValidationAlert_1.default, null, 'Enter your email'),
-                            _this.state.error.emailValid &&
-                                React.createElement(ValidationAlert_1.default, null, 'Wrong email format'),
-                            _this.state.error.sending &&
-                                React.createElement(ValidationAlert_1.default, null, 'Error sending email.'),
-                            _this.state.displayThankYou && (React.createElement("div", { className: 'subscribe__thankyou' }, "Thanks for subscribing to our newsletter."))),
                         React.createElement("div", { className: 'footer__divider' }),
                         React.createElement("div", { className: 'footer__navigation row d-flex justify-content-between align-items-start' },
                             React.createElement("nav", { className: 'footer__navigation__item col-12 col-md-6 col-xl' },
