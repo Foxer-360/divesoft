dc407c63b57b5a2525619018cddfcb39c699d1de
Igor Šula <sulaigor@gmail.com>
2021-02-25 11:11:41 +0100

    Refactor carousel fifty fifty component utils

--- src/components/CarouselFiftyFifty/utils.ts
+++ src/components/CarouselFiftyFifty/utils.ts
@@ -1,6 +1,5 @@
 import { IMAGE_POSITION_RIGHT, IMAGE_POSITION_LEFT } from './const';
 import { ImagePositionType } from './types';
 
-export const getImagePosition = (imageOnRight): ImagePositionType => {
-  return imageOnRight ? IMAGE_POSITION_RIGHT : IMAGE_POSITION_LEFT;
-};
+export const getImagePosition = (imageOnRight): ImagePositionType =>
+  imageOnRight ? IMAGE_POSITION_RIGHT : IMAGE_POSITION_LEFT;
