32039b6adccc5ae07cba8c515791c6d3dd74e9b0
Ondřej Rejnek <ondrej.rejnek@gmail.com>
2021-02-25 11:28:08 +0100

    Fix default import in get image url

--- src/helpers/getImageUrl.tsx
+++ src/helpers/getImageUrl.tsx
@@ -1,9 +1,11 @@
 import { BASE_URL } from '@source/const/mediaLibrary';
 
-export const getImgUrl = (data) => {
+const getImageUrl = (data) => {
   if (data && data.filename) {
     return BASE_URL + data.category + data.hash + '_' + data.filename;
   }
 
   return null;
 };
+
+export default getImageUrl;
