378327e869143c0c76346bee6f6dd62b98f79a78
Igor Šula <sulaigor@gmail.com>
2021-03-21 00:21:21 +0100

    Add product preview component

---
+++ src/components/ProductMicrosite/MicrositeContent/ProductPreview/ProductPreview.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const ProductPreview = () => {
+  return (
+    <div className="productMicrosite__productPreview">
+      <img
+        className="productMicrosite__productPreview__image"
+        src="/assets/divesoft/images/freedom.png"
+        alt="Product preview"
+      />
+    </div>
+  );
+};
+
+export default ProductPreview;
