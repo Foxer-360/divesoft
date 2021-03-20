8a853c17ea7c7f10f6d7ef0c294728306ece955a
Igor Šula <sulaigor@gmail.com>
2021-03-20 22:05:13 +0100

    Add microsite header and container components to product microsite

--- src/components/ProductMicrosite/ProductMicrosite.tsx
+++ src/components/ProductMicrosite/ProductMicrosite.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import Container from '@source/partials/Container';
+import MicrositeHeader from './MicrositeHeader';
 
 const ProductMicrosite = () => {
-  return <div>ProductMicrosite</div>;
+  return (
+    <div className="productMicrosite">
+      <MicrositeHeader />
+      <Container>ProductMicrosite</Container>
+    </div>
+  );
 };
 
 export default ProductMicrosite;
