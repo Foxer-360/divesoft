7f8ca69ee66797893f7b98e0076c8c9441bbd550
Igor Šula <sulaigor@gmail.com>
2021-03-21 21:18:12 +0100

    Add title component to microsite content module

---
+++ src/components/ProductMicrosite/MicrositeContent/Title/Title.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { ITextProps as IProps } from '../types';
+
+const Title = ({ children, className }: IProps) => {
+  return <h2 className={`productMicrosite__title ${className}`}>{children}</h2>;
+};
+
+export default Title;
