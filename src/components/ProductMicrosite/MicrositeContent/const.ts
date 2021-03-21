22cc16a0d1cca21a451efacb4e6af7373b7a467d
Igor Šula <sulaigor@gmail.com>
2021-03-22 00:37:43 +0100

    Add microsite content component contants

---
+++ src/components/ProductMicrosite/MicrositeContent/const.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_TITLE = 'Úvodní text který se ukáže po odscrollování zařízení';
+export const DEFAULT_SUBTITLE = 'Inovativní zobrazení během ponoru';
+export const DEFAULT_TEXT =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur magna libero, ultricies finibus interdum at, consequat in ante. Nam vehicula iaculis nulla ut vestibulum.';
