806742956a174075f0c97a03a5ae7331610d57c1
Igor Šula <sulaigor@gmail.com>
2021-03-24 23:35:22 +0100

    Add default class name props value in title component

--- src/components/ProductMicrosite/MicrositeContent/Title/Title.tsx
+++ src/components/ProductMicrosite/MicrositeContent/Title/Title.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ITextProps as IProps } from '../types';
 
-const Title = ({ children, className }: IProps) => {
+const Title = ({ children, className = '' }: IProps) => {
   return <h2 className={`productMicrosite__title ${className}`}>{children}</h2>;
 };
 
