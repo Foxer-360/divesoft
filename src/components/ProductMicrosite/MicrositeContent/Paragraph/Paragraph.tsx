9b87b0a70dcae71ba497f38a77d7669000e95e24
Igor Šula <sulaigor@gmail.com>
2021-03-24 23:34:59 +0100

    Add default class name props value in paragraph component

--- src/components/ProductMicrosite/MicrositeContent/Paragraph/Paragraph.tsx
+++ src/components/ProductMicrosite/MicrositeContent/Paragraph/Paragraph.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ITextProps as IProps } from '../types';
 
-const Paragraph = ({ children, className }: IProps) => {
+const Paragraph = ({ children, className = '' }: IProps) => {
   return <p className={`productMicrosite__paragraph ${className}`}>{children}</p>;
 };
 
