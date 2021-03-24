7f43d95d1d627e65e5caa159cd8f16a1bdf26dc0
Igor Šula <sulaigor@gmail.com>
2021-03-25 00:09:41 +0100

    Add type property to product preview component

--- src/components/ProductMicrosite/MicrositeContent/ProductPreview/ProductPreview.tsx
+++ src/components/ProductMicrosite/MicrositeContent/ProductPreview/ProductPreview.tsx
@@ -2,17 +2,26 @@ import * as React from 'react';
 import Container from '@source/partials/Container';
 import ContentCard, { IContentCardProps } from '../ContentCard';
 
-interface IProps extends IContentCardProps {}
+enum PRODUCT_PREVIEW_TYPE_ENUM {
+  default = 'default',
+  small = 'small',
+}
 
-const ProductPreview = ({ title, text }: IProps) => {
+interface IProps extends IContentCardProps {
+  type?: keyof typeof PRODUCT_PREVIEW_TYPE_ENUM;
+}
+
+const ProductPreview = ({ title, text, type = PRODUCT_PREVIEW_TYPE_ENUM.default }: IProps) => {
   return (
     <Container>
-      <section className="productMicrosite__productPreview">
-        <img
-          className="productMicrosite__productPreview__image"
-          src="/assets/divesoft/images/freedom.png"
-          alt="Product preview"
-        />
+      <section className={`productMicrosite__productPreview ${type}`}>
+        <div className="productMicrosite__productPreview__imageWrapper">
+          <img
+            className="productMicrosite__productPreview__image"
+            src="/assets/divesoft/images/freedom.png"
+            alt="Product preview"
+          />
+        </div>
         <ContentCard title={title} text={text} />
       </section>
     </Container>
