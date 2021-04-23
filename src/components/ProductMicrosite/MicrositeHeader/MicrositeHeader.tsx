b9a4053d48a607ccd710883a547e862e96bdccde
Igor Šula <sulaigor@gmail.com>
2021-04-24 00:19:30 +0200

    Add usage of resource data in product microsite header

--- src/components/ProductMicrosite/MicrositeHeader/MicrositeHeader.tsx
+++ src/components/ProductMicrosite/MicrositeHeader/MicrositeHeader.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import HeaderLogo from '@source/partials/HeaderLogo';
 import Button from '@source/partials/Button';
+import Subtitle from '../MicrositeContent/Subtitle';
 
-const MicrositeHeader = () => {
+interface IProps {
+  productName: string;
+  productUrl: string;
+}
+
+const MicrositeHeader = ({ productName, productUrl }: IProps) => {
   return (
     <header className="productMicrosite__header">
       <HeaderLogo url="/en" />
-      <Button>Objednat</Button>
+      <Subtitle>{productName}</Subtitle>
+      <a href={productUrl} target="__blank">
+        <Button>Objednat</Button>
+      </a>
     </header>
   );
 };
--- src/components/ProductMicrosite/ProductMicrosite.tsx
+++ src/components/ProductMicrosite/ProductMicrosite.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import MicrositeHeader from './MicrositeHeader';
 import MicrositeContent from './MicrositeContent';
+import { IProductMicrositeData } from './types';
 
-const ProductMicrosite = () => {
+interface IProps {
+  data: IProductMicrositeData;
+}
+
+const ProductMicrosite = ({ data }: IProps) => {
+  const { productName, productUrl } = data;
+
   return (
     <div className="productMicrosite">
-      <MicrositeHeader />
+      <MicrositeHeader productName={productName} productUrl={productUrl} />
       <MicrositeContent />
     </div>
   );
