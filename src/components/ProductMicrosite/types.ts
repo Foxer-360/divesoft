7dda4f8a2ccef185c05916b1ff645e4c59f547a8
Igor Šula <sulaigor@gmail.com>
2021-04-24 00:18:48 +0200

    Add product microsite component resource type

---
+++ src/components/ProductMicrosite/types.ts
@@ -0,0 +1,23 @@
+interface IFifthSectionItem {
+  title: string;
+  description: string;
+}
+
+export interface IProductMicrositeData {
+  productName: string;
+  productPrice: string;
+  productUrl: string;
+  firstSectionTitle: string;
+  firstSectionDescription: string;
+  secondSectionTitle: string;
+  secondSectionDescription: string;
+  thirdSectionTitle: string;
+  thirdSectionDescription: string;
+  fourthSectionTitle: string;
+  fourthSectionDescription: string;
+  fifthSectionTitle: string;
+  fifthSectionDescription: string;
+  fifthSectionItems: IFifthSectionItem[];
+  sixthSectionTitle: string;
+  sixthSectionDescription: string;
+}
